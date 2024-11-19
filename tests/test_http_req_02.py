28d5ca1b53495b4891fdeee927df308fce4a504b
kbhalerao-vonage <kaustubh.bhalerao@vonage.com>
2024-11-19 16:57:21 +0530

    Added another test file with name test_http_req_02.py

---
+++ tests/test_http_req_02.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from resources.status_code import StatusCode
+from source.http_req import HttpReq
+
+
+class TestHttpReq(unittest.TestCase):
+    """
+    A class that contains test methods (inherited from unittest.TestCase)
+    to send HTTP Request and checks HTTP Response sent by server
+    """
+
+    def setUp(self) -> None:
+        self.reqObj = HttpReq()
+        self.response = None
+
+    def tearDown(self) -> None:
+        del self.response
+
+    def test_001_http_get_request(self):
+        print("This is a test file created for testing)
+
+
